4e5d8a47dc3491de9ee984014d405e7e4865d5fb
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2023-07-11 17:55:03 +0000

    Add changes

--- src/components/PromptToLocation.jsx
+++ src/components/PromptToLocation.jsx
@@ -1,6 +1,21 @@
 import PropTypes from "prop-types";
 
 const PromptToLocation = (prompt) => {
+  const url = "https://api.openai.com/v1/chat/completions";
+
+  const data = {
+    model: "gpt-3.5-turbo-0613",
+    messages: [{ role: "user", content: prompt }],
+  };
+  const params = {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_OPENAI}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    method: "POST",
+  };
+
   return fetch(url, params)
     .then((response) => response.json())
     .then((data) => {
--- src/components/useApiRequests.jsx
+++ src/components/useApiRequests.jsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import LocationToCoordinates from "./LocationToCoordinates";
 import WeatherData from "./WeatherData";
+import PromptToLocation from "./PromptToLocation";
 
 const useApiRequests = (prompt) => {
   const [error, setError] = useState(null);
+  const [promptData, setPromptData] = useState({});
   const [locationData, setLocationData] = useState([]);
   const [weatherData, setWeatherData] = useState({});
 
@@ -14,6 +16,9 @@ const useApiRequests = (prompt) => {
       if (!prompt) return; // return if prompt is null or undefined
 
       try {
+        const promptDataRes = await PromptToLocation(prompt);
+        setPromptData(promptDataRes);
+
         const locationDataRes = await LocationToCoordinates(prompt);
         setLocationData(locationDataRes);
 
