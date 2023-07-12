35c717fde7ed1402ba2b885a34050b99b7a98532
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2023-07-12 21:07:14 +0000

    add changes

--- src/components/useApiRequests.jsx
+++ src/components/useApiRequests.jsx
@@ -3,12 +3,14 @@ import PropTypes from "prop-types";
 import LocationToCoordinates from "./LocationToCoordinates";
 import WeatherData from "./WeatherData";
 import PromptToLocation from "./PromptToLocation";
+import WeatherDescript from "./WeahterDescript";
 
 const useApiRequests = (prompt) => {
   const [error, setError] = useState(null);
   const [promptData, setPromptData] = useState({});
   const [locationData, setLocationData] = useState([]);
   const [weatherData, setWeatherData] = useState({});
+  const [weatherDescription, setWeatherDescription] = useState(null);
 
   // Fetch location and weather data from API.
   useEffect(() => {
@@ -26,6 +28,12 @@ const useApiRequests = (prompt) => {
 
         const weatherDataRes = await WeatherData(locationDataRes);
         setWeatherData(weatherDataRes);
+
+        const weatherDescriptRes = await WeatherDescript(
+          prompt,
+          weatherDataRes
+        );
+        setWeatherDescription(weatherDescriptRes);
       } catch (error) {
         setError(error);
         console.error("Error:", error);
