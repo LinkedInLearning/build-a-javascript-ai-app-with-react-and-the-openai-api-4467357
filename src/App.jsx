ef04709f22a3ac80a1e2620fb91e4b7990a28129
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2023-07-13 16:25:54 +0000

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const [prompt, setPrompt] = useState("");
   const [units, setUnits] = useState("metric");
   const [weatherDataLoading, setWeatherDataLoading] = useState(false);
+  const [weatherDescriptLoading, setWeatherDescriptLoading] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
 
   // Custom hook to handle API requests. Fires when prompt changes.
@@ -31,6 +32,12 @@ function App() {
   }, [weatherData]);
 
   useEffect(() => {
+    if (weatherDescription) {
+      setWeatherDescriptLoading(false);
+    }
+  }, [weatherDescription]);
+
+  useEffect(() => {
     if (promptData && promptData.units) {
       setUnits(promptData.units);
     }
@@ -40,6 +47,7 @@ function App() {
   const handleSubmit = (newPrompt) => {
     setErrorMsg("");
     setWeatherDataLoading(true);
+    setWeatherDescriptLoading(true);
     setPrompt(newPrompt);
   };
 
@@ -50,9 +58,12 @@ function App() {
         <WeatherForm onSubmit={handleSubmit} />
         {error && <p className="error">{errorMsg.message}</p>}
         {weatherDescription ? (
-          <Description weatherDescription={weatherDescription} />
+          <Description
+            isLoading={weatherDescriptLoading}
+            weatherDescription={weatherDescription}
+          />
         ) : (
-          <Description />
+          <Description isLoading={weatherDescriptLoading} />
         )}
       </header>
       <main className="main-content">
--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types";
 import "./Description.css";
+import Loader from "./Loader";
 
-const Description = ({ weatherDescription }) => {
+const Description = ({ isLoading, weatherDescription }) => {
   return (
     <div className="description">
       <h2 className="description__title">Description</h2>
       <div className="description__divider">
+        {isLoading && <Loader />}
         <p className="description__text">{weatherDescription}</p>
       </div>
     </div>
